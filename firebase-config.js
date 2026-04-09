f8e4f7aa11a256c3887cabdb331c35550a42fd2e
Nell44 <nelly.kamdem@efrei.net>
2026-04-09 07:23:56 +0200

    Set up Firebase configuration and services
    Added Firebase configuration and initialization code.

--- firebase-config.js
+++ firebase-config.js
@@ -1,1 +1,31 @@
-<!-- vide -->
+// Import des modules Firebase (v10 via CDN)
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
+
+// Ma configuration Firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyAX-TZmfRgn0kf6JjH5WymxVXtie6bb5wc",
+  authDomain: "lighther-694b3.firebaseapp.com",
+  projectId: "lighther-694b3",
+  storageBucket: "lighther-694b3.firebasestorage.app",
+  messagingSenderId: "239266315207",
+  appId: "1:239266315207:web:ae9667176658daf88277e2",
+  measurementId: "G-PGKJMRGTSG"
+};
+
+
+//  Initialisation Firebase
+const app = initializeApp(firebaseConfig);
+
+
+//  Services
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+
+//  Export
+export { auth, db };
